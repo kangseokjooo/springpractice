be4d31b96dea9a9ed2059d83292f8865065faa54
kangseokjooo <zpckq69@naver.com>
2024-09-26 10:43:44 +0900

    SpringSecurity JWT Description

--- src/main/java/com/prac/Prac/Config/SecurityConfig.java
+++ src/main/java/com/prac/Prac/Config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.prac.Prac.Config;
 
 import com.prac.Prac.Util.JwtFilter;
-import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
